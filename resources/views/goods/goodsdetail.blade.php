a1025302b55389fa93aa175ffcc7bfdf3fe3b076
nxxxiu <1623248430@qq.com>
2019-04-24 05:52:17 +0000

    分

--- resources/views/goods/goodsdetail.blade.php
+++ resources/views/goods/goodsdetail.blade.php
@@ -103,7 +103,7 @@
         timestamp:"{{$jsconfig['timestamp']}}" , // 必填，生成签名的时间戳
         nonceStr:"{{$jsconfig['nonceStr']}}", // 必填，生成签名的随机串
         signature: "{{$jsconfig['signature']}}",// 必填，签名
-        jsApiList: ['updateAppMessageShareData','updateTimelineShareData','onMenuShareTimeline','onMenuShareAppMessage','chooseImage','uploadImage'] // 必填，需要使用的JS接口列表
+        jsApiList: ['updateAppMessageShareData','updateTimelineShareData','chooseImage','uploadImage'] // 必填，需要使用的JS接口列表
     });
 
     wx.ready(function () {   //需在用户可能点击分享按钮前就先调用
@@ -125,25 +125,6 @@
             success: function () {
                 // 设置成功
                 // alert('分享成功');
-            }
-        })
-        wx.onMenuShareTimeline({
-            title: '旺旺', // 分享标题
-            link: 'http://1809niqingxiu.comcto.com/goodsdetail/' + "{{$data['goods_id']}}", // 分享链接，该链接域名或路径必须与当前页面对应的公众号JS安全域名一致
-            imgUrl: 'http://1809niqingxiu.comcto.com/img/okk.jpg', // 分享图标
-            success: function () {
-                // 用户点击了分享后执行的回调函数
-            },
-        })
-        wx.onMenuShareAppMessage({
-            title: '旺仔', // 分享标题
-            desc: 'emmmm....', // 分享描述
-            link: 'http://1809niqingxiu.comcto.com/goodsdetail/'+"{{$data['goods_id']}}", // 分享链接，该链接域名或路径必须与当前页面对应的公众号JS安全域名一致
-            imgUrl: 'http://1809niqingxiu.comcto.com/img/okk.jpg', // 分享图标
-            type: '', // 分享类型,music、video或link，不填默认为link
-            dataUrl: '', // 如果type是music或video，则要提供数据链接，默认为空
-            success: function () {
-// 用户点击了分享后执行的回调函数
             }
         });
     });
