3f65ed3a0969c738f7fae7fa3a8ec3dc6d2146dc
nxxxiu <1623248430@qq.com>
2019-04-24 01:51:23 +0000

    model

--- app/Http/Controllers/WeixinController.php
+++ app/Http/Controllers/WeixinController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 use GuzzleHttp\Client;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use App\WxText;
+use App\WxUser;
 class WeixinController extends Controller
 {
     public function valid()
