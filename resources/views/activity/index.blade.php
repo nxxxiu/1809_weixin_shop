1378dac67f33e31ec534bc02e8690666a4f3f22a
nxxxiu <1623248430@qq.com>
2019-04-28 10:32:27 +0000

    bug

--- resources/views/activity/index.blade.php
+++ resources/views/activity/index.blade.php
@@ -15,7 +15,7 @@
 <script src="http://res2.wx.qq.com/open/js/jweixin-1.4.0.js"></script>
 <script>
     wx.config({
-        debug: true, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
+        // debug: true, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
         appId: "{{$jsconfig['appId']}}", // 必填，公众号的唯一标识
         timestamp:"{{$jsconfig['timestamp']}}" , // 必填，生成签名的时间戳
         nonceStr:"{{$jsconfig['nonceStr']}}", // 必填，生成签名的随机串
@@ -24,7 +24,7 @@
     })
     wx.ready(function () {   //需在用户可能点击分享按钮前就先调用
         wx.updateAppMessageShareData({
-            title: 'Brwown bear', // 分享标题
+            title: 'Brown bear', // 分享标题
             desc: 'bear', // 分享描述
             link: 'http://1809niqingxiu.comcto.com/activity/index', // 分享链接，该链接域名或路径必须与当前页面对应的公众号JS安全域名一致
             imgUrl: 'http://1809niqingxiu.comcto.com/img/ok.jpg', // 分享图标
