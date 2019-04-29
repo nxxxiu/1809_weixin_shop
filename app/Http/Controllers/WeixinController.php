e4292f45963352391286f2eb98a0e40c5ac7022a
nxxxiu <1623248430@qq.com>
2019-04-29 11:45:29 +0000

    签到

--- app/Http/Controllers/WeixinController.php
+++ app/Http/Controllers/WeixinController.php
@@ -235,7 +235,7 @@ class WeixinController extends Controller
                     'url'=> $url,
                 ],
                 [
-                    'type'=>'click',
+                    'type'=>'view',
                     'name'=>'签到',
                     'url'=>$url_signin,
                 ]
